867a256d1e126274cf2a328d3414b731a9f73a5e
hark17 <harjeevs17@gmail.com>
2020-08-02 00:40:22 +0530

    version2

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,6 +7,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import emailjs from "emailjs-com";
+import { Link } from "react-router-dom";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import { FaGoogle, FaFacebook, FaShareAlt } from "react-icons/fa";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
 import {
   FacebookShareButton,
   InstapaperShareButton,
@@ -18,7 +25,6 @@ import {
   TumblrShareButton,
   TwitterShareButton,
   WhatsappShareButton,
-  FacebookIcon,
 } from "react-share";
 
 import styles from "./Home.css";
@@ -47,6 +53,13 @@ const useStyles = makeStyles((theme) => ({
   intro: {
     fontSize: 20,
   },
+  mainBox: {
+    height: "80vh",
+    flexDirection: "column",
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+  },
 }));
 
 const Main = () => {
@@ -55,6 +68,7 @@ const Main = () => {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [pop, setPop] = useState(false);
   const [open, setOpen] = useState(false);
   const [btn, setbtn] = useState("Send Message");
   const classes = useStyles();
@@ -74,6 +88,7 @@ const Main = () => {
   };
   const submitData = () => {
     console.log(name);
+    // setPop(true);
     setbtn("Loading");
     const html = `<div>
     <p>The name is ${name}
@@ -89,6 +104,7 @@ const Main = () => {
           reply_to: "harjeevs17@gmail.com",
           to_name: "Harjeev",
           from_name: "noreply@gmail.com",
+          sender: name,
           message_html: html,
         },
         "user_8fCHR8UMMvdJCNYX8uhVe"
@@ -96,6 +112,7 @@ const Main = () => {
       .then(
         (result) => {
           setbtn("Send Message");
+          setPop(true);
         },
         (error) => {
           console.log(error.text);
@@ -104,13 +121,17 @@ const Main = () => {
   };
 
   return (
-    <div>
+    <div className={classes.mainBox}>
       <div>
         <p style={{ margin: 20 }}>
           Help us. Help others. You’re invited to review:
         </p>
         <h1 style={{ padding: 10, fontSize: 50, color: "#1b75bb" }}>
-          YOUR LOGO HERE
+          <img
+            className="responsiveImg"
+            src={require("../assets/logo.jpg")}
+            alt="Logo"
+          />
         </h1>
         <Typography
           className={classes.intro}
@@ -122,7 +143,7 @@ const Main = () => {
       </div>
       <div className="stars">
         <Rating
-          style={{ fontSize: 70 }}
+          style={{ fontSize: 70, paddingTop: 20 }}
           name="simple-controlled"
           onChange={(event, newValue) => {
             setValue(newValue);
@@ -205,15 +226,20 @@ const Main = () => {
                     experience on one of these sites?
                   </p>
                   <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div className={classes.icons}>
-                      <FacebookShareButton url="facebook.com" quote="hihihi">
-                        <FacebookIcon size={32} round />
-                      </FacebookShareButton>
+                    <div>
+                      <a href="https://www.facebook.com/pg/hhsolarpower/reviews/?ref=page_internal">
+                        <FaFacebook fontSize="35px" style={{ padding: 10 }} />
+                      </a>
                     </div>
-                    <div className={classes.icons}>
-                      <FacebookShareButton url="facebook.com" quote="hihihi">
-                        <FacebookIcon size={32} round />
-                      </FacebookShareButton>
+                    <div>
+                      <a href="https://www.google.com/search?q=hilton+head+solar&rlz=1CAOTWH_enUS879&oq=hilton+head+solar&aqs=chrome.0.69i59j46j0l3j69i61j69i60l2.1961j0j7&sourceid=chrome&ie=UTF-8#lrd=0x88fbf35b9699e481:0xe6a3b9ff1a25b1b5,3">
+                        <FaGoogle fontSize="35px" style={{ padding: 10 }} />
+                      </a>
+                    </div>
+                    <div>
+                      <a href="https://www.solarreviews.com/installers/hilton-head-solar-reviews">
+                        <FaShareAlt fontSize="35px" style={{ padding: 10 }} />
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -222,8 +248,28 @@ const Main = () => {
           )}
         </div>
       </Modal>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={pop}
+        autoHideDuration={6000}
+        onClose={() => setPop(false)}
+        message="Successully submitted"
+      />
     </div>
   );
 };
 
 export default Main;
+/* <div className={classes.icons}>
+                      <FacebookShareButton url="facebook.com" quote="hihihi">
+                        <FacebookIcon size={32} round />
+                      </FacebookShareButton>
+                    </div>
+                    <div className={classes.icons}>
+                      <FacebookShareButton url="facebook.com" quote="hihihi">
+                        <FacebookIcon size={32} round />
+                      </FacebookShareButton>
+                    </div>*/
